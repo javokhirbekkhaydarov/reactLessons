92ef9d47d99acc9160e2525e9f975c0d64d69682
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-15 00:58:28 +0500

    test one

---
+++ lesson1/js/app.js
@@ -0,0 +1,9 @@
+//! template literal ====
+
+const fName = "Javokhirbek";
+const job = "Developer";
+const birthDay = 2003;
+const year = 2022;
+
+const me = " I'm " + fName + " , a" + job
+console.log(me);
