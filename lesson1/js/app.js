7dae8723b816e456dfb2420982ea3fcd1bbe6069
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-15 01:26:20 +0500

    ADDED MAP

--- lesson1/js/app.js
+++ lesson1/js/app.js
@@ -5,25 +5,36 @@ const job = "Developer";
 const birthDay = 2003;
 const year = 2022;
 
-const me = " I'm " + fName + " , a" + job
+const me = " I'm " + fName + " , a" + job;
 console.log(me);
 
-
-
-const newMe = `I'm ${fName} , ${job}, i'm ${year - birthDay} age`
+const newMe = `I'm ${fName} , ${job}, i'm ${year - birthDay} age`;
 console.log(newMe);
-
-
 
 //! if else statiments
 
-const myAge = 19
+const myAge = 19;
 const yearsLesft = 18 - myAge;
-if(myAge >= 18) {
-    console.log("haydolaman");
-}
-else {
-    console.log("uxlaaa");
+if (myAge >= 18) {
+  console.log("haydolaman");
+} else {
+  console.log("uxlaaa");
 }
 
-myAge  >= 18 ? console.log("im ready") : console.log("okay");;
+myAge >= 18 ? console.log("im ready") : console.log("okay");
+
+// !array
+
+movements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+euroToUsed = 1.1;
+
+const movementToUsd = movements.map((mow) => mow * euroToUsed);
+
+console.log(movementToUsd);
+
+movements.map((mov, index) => {
+  if (mov > 0) {
+    console.log("udAE  ");
+  }
+});
