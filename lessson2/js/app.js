3f0dac0d91a7669991d5ab4ab8c68b1c3b948e61
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-15 23:36:16 +0500

    add destructed

--- lessson2/js/app.js
+++ lessson2/js/app.js
@@ -17,3 +17,35 @@ const balance = movements.reduce(function (
   return prevValue + currentValue;
 });
 console.log(balance);
+
+//! destructuring that is edit code structure
+
+const clubs = {
+  name: "Real Madrid",
+  location: "Madrid",
+  categories: ["Spain", "Men", "Women"],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+};
+
+const arr = [2, 3, 4];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a,b,c);
+
+
+//! destruct qilishdan maqsad har bitta elementni alohida ishlsh imkoniga ega
