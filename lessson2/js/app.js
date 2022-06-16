fea1408b0ed8208b27780bfb331128a42748cf5f
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-16 05:08:16 +0500

    add spread operators

--- lessson2/js/app.js
+++ lessson2/js/app.js
@@ -40,12 +40,25 @@ const clubs = {
   },
 };
 
+const { name: clubName, openingHours: hours, categories: tags } = clubs;
+console.log(clubName, hours, tags);
 const arr = [2, 3, 4];
 
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
-console.log(a,b,c);
-
+console.log(a, b, c);
 
 //! destruct qilishdan maqsad har bitta elementni alohida ishlsh imkoniga ega
+
+//!spread operators
+
+const arrSpread = [7, 8, 9];
+
+const badNewArr = [1, 2, arrSpread[0], arrSpread[1], arrSpread[2]];
+console.log(badNewArr);
+const newArr = [1, 2, ...arrSpread];
+console.log(...arrSpread);
+
+const newCategory = [...clubs.categories , "Barsa"]
+console.log(newCategory);
